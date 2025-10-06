c6f07ca6800c63a1aef090d00e241114ace463cc
DeveshJakhmola16 <dpjakhmola@gmail.com>
2025-10-06 22:13:08 +0530

    Size.c

--- Size.c
+++ Size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
     printf("Size of char: %zu byte(s)\n", sizeof(char));
     printf("Size of int: %zu byte(s)\n", sizeof(int));
     printf("Size of short: %zu byte(s)\n", sizeof(short));
@@ -8,4 +8,5 @@ int main(void) {
     printf("Size of float: %zu byte(s)\n", sizeof(float));
     printf("Size of double: %zu byte(s)\n", sizeof(double));
     return 0;
+
 }
