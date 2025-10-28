78d4d19380e8efaeaa45281694c105b2f38a68ba
DeveshJakhmola16 <dpjakhmola@gmail.com>
2025-10-28 11:55:45 +0530

    Add files via upload

---
+++ Reverse_String.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char str[100];
+    int i, length;
+    printf("Enter a string: ");
+    scanf("%s", str);  // reads input until space
+    length = strlen(str);
+    printf("Reversed string: ");
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    return 0;
+}
